af8b9542d40554c40ca780ad38b92c6cb0240b47
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-02-01 06:42:49 +0530

    Create Print_2D_Arrays.java

---
+++ Print_2D_Arrays.java
@@ -0,0 +1,38 @@
+// 2D arrays Basics
+import java.util.Scanner;
+class Main{
+    public static void main(String []k){
+        Scanner s1 = new Scanner(System.in);
+        int row = s1.nextInt();
+        int column = s1.nextInt();
+        
+        int arr[][] = new int [row][column];
+        
+        // Taking input of the 2D Arrays
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < column; j++){
+                arr[i][j] = s1.nextInt();
+            }
+        }
+        
+        // Just print the 2D Arrays Using Nested Loop
+        System.out.println();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < column; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        
+        // Opposite way print start with column end with row
+        
+        System.out.println();
+        for(int j = 0; j < column; j++){
+            for(int i = 0; i < row; i++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
